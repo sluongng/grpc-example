4284b066cc0be40fa8889e4e774d5a6e374e7599
Son Luong Ngoc (Lazada Group) <son.luongngoc@lazada.com>
2018-05-20 18:52:30 +0700

    Implemented Role Service

--- role-service/go.mod
+++
@@ -1,3 +0,0 @@
-module awesomeProject
-
-require github.com/gorilla/mux v1.6.2
--- role-service/main.go
+++ role-service/main.go
@@ -1,15 +1,75 @@
 package user_service
 
 import (
-	"v/github.com/gorilla/mux@v1.6.2"
-	"awesomeProject/user-service/pb"
+	pb "github.com/sluongng/grpc-example/role-service/pb"
+	"net"
+	"log"
+	"google.golang.org/grpc"
+	"context"
+	"errors"
 )
 
 type Server struct {
+	userRoles map[int32][]*pb.Role
 	roles []*pb.Role
 }
 
+func (s *Server) GetRoles(_ context.Context, _ *pb.EmptyRequest) (*pb.RolesReply, error) {
+	return &pb.RolesReply{
+		Roles: s.roles,
+	}, nil
+}
+
+func (s *Server) GetUserRole( _ context.Context, req *pb.GetUserRoleRequest) (*pb.UserRoleReply, error) {
+	roles, ok := s.userRoles[req.UserId]
+	if !ok {
+		return nil, errors.New("user not found")
+	}
+
+	return &pb.UserRoleReply{
+		UserId: req.UserId,
+		Roles: roles,
+	}, nil
+}
+
 func main() {
-	r := mux.NewRouter()
-	_ = r
+	var (
+		normal = &pb.Role{
+			Id: 1,
+			Name: "normal",
+		}
+		manager = &pb.Role{
+			Id: 1,
+			Name: "manager",
+		}
+		cto = &pb.Role{
+			Id: 1,
+			Name: "CTO",
+		}
+		ceo = &pb.Role{
+			Id: 1,
+			Name: "CEO",
+		}
+	)
+
+	lis, err := net.Listen("tcp", "localhost:7060")
+	if err != nil {
+		log.Fatalf("failed to initialize TCP listerner: %v", err)
+	}
+	defer  lis.Close()
+
+	server := grpc.NewServer()
+	roleServer := &Server{
+		userRoles: map[int32][]*pb.Role{
+			1: {normal},
+			2: {normal, manager},
+			3: {manager},
+			4: {cto},
+			5: {ceo},
+		},
+		roles: []*pb.Role{normal, manager, cto, ceo},
+	}
+	pb.RegisterRolesServer(server, roleServer)
+
+	server.Serve(lis)
 }
